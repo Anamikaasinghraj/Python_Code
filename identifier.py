92612d944bd1015a821e15f57b5d446b6bdb0bad
Anamika Singh Raj <anamikasinghrajraj@gmail.com>
2024-12-09 14:08:31 +0530

    Add files via upload

---
+++ identifier.py
@@ -0,0 +1,11 @@
+# ***Identifiers*** :
+
+ # A Python is a name used to identify a variable, function, class, module or other object.
+#code:
+
+name = "anamika"
+a = 10
+b = 1.2
+print(name)
+print(a)
+print(b)
---
+++ test.py
@@ -0,0 +1,5 @@
+#starting code of python 
+ 
+a = 2
+b = 10
+print(a + b)
